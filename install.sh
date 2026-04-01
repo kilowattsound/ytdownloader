723ce3aa32a47c84da3b4a61bcb37a75abd37f4c
kilowattsound <ciorniipavel@gmail.com>
2026-04-01 18:02:50 -0500

    Fix formatting in install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
-BOLD='\033[1m'
+BOLD='\033[1m' 
 NC='\033[0m'
 
 echo -e "${BLUE}${BOLD}--- YouTube Downloader Installer ---${NC}"
